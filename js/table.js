0fd5418b2d6580b07b734e755c058c5dc99def05
kryptiksage <kryptiksage@gmail.com>
2021-08-18 15:04:04 +0530

    js/table.js : fixed indentation

--- js/table.js
+++ js/table.js
@@ -1,4 +1,26 @@
-export function displayGraph(confirmed, nconfirmed, recovered, nrecovered, tested, ntested, deceased) {
-
-    document.getElementById("tableData").innerHTML = '<td>' + nconfirmed + '</td><td>' + nrecovered + '</td><td>' + ntested + '</td><td>' + confirmed + '</td><td>' + deceased + '</td><td>' + recovered + '</td><td>' + tested + '</td>';
+export function displayGraph(
+	confirmed,
+	nconfirmed,
+	recovered,
+	nrecovered,
+	tested,
+	ntested,
+	deceased
+) {
+	document.getElementById("tableData").innerHTML =
+		"<td>" +
+		nconfirmed +
+		"</td><td>" +
+		nrecovered +
+		"</td><td>" +
+		ntested +
+		"</td><td>" +
+		confirmed +
+		"</td><td>" +
+		deceased +
+		"</td><td>" +
+		recovered +
+		"</td><td>" +
+		tested +
+		"</td>";
 }

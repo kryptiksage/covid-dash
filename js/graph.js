278fb91c3e09a75678fcce7d283e18765d556128
GowriLakshmiDevarajan <gowrilakshmidevarajan99@gmail.com>
2021-08-18 12:10:17 +0530

    Chart function added

---
+++ js/graph.js
@@ -0,0 +1,39 @@
+function selected(select,state,dated,confirmed,recovered,deceased)
+    {
+var spreadCanvas = document.getElementById("spreadChart");
+var lineChart = new Chart(spreadCanvas, {
+  type: 'line',
+  data:{
+  labels:dated,
+  datasets:[{
+    label: "Confirmed",
+    data: confirmed,
+    
+    borderColor: 'red'
+  },
+  {
+    label: "Recovered",
+    data:recovered,
+    
+  
+  borderColor: 'blue'
+  },
+  {
+    label: "Deceased",
+    data:deceased,
+    
+  
+  borderColor: 'yellow'
+  }]},
+  options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+
+  
+});
+    }
+   

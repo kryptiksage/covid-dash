549667149f18d67c043f9308acfbf4d68b7c5358
kryptiksage <kryptiksage@gmail.com>
2021-08-18 12:52:51 +0530

    js/graph.js : added indentation

--- js/graph.js
+++ js/graph.js
@@ -1,77 +1,36 @@
-function selected(dated,confirmed,recovered,deceased)
-    {
-var spreadCanvas = document.getElementById("spreadChart");
-var lineChart = new Chart(spreadCanvas, {
-  type: 'line',
-  data:{
-  labels:dated,
-  datasets:[{
-    label: "Confirmed",
-    data: confirmed,
-    
-    borderColor: 'red'
-  },
-  {
-    label: "Recovered",
-    data:recovered,
-    
-  
-  borderColor: 'blue'
-  },
-  {
-    label: "Deceased",
-    data:deceased,
-    
-  
-  borderColor: 'yellow'
-  }]},
-  options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
+function selected(dated, confirmed, recovered, deceased) {
+	var spreadCanvas = document.getElementById("spreadChart");
+	var lineChart = new Chart(spreadCanvas, {
+		type: "line",
+		data: {
+			labels: dated,
+			datasets: [
+				{
+					label: "Confirmed",
+					data: confirmed,
 
-  
-});
-    }
-   
+					borderColor: "red",
+				},
+				{
+					label: "Recovered",
+					data: recovered,
 
+					borderColor: "blue",
+				},
+				{
+					label: "Deceased",
+					data: deceased,
 
-
-
-
-
-
-
-
-    
-
-
-
-
-
-    
-  
-
-
-
-
-
-    
-  
-
-
-
-
-
-
-
-
-
-
-  
-
-
-   
+					borderColor: "yellow",
+				},
+			],
+		},
+		options: {
+			scales: {
+				y: {
+					beginAtZero: true,
+				},
+			},
+		},
+	});
+}
